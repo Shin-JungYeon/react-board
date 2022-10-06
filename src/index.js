608df2b258c80b860febb86175068181b97ad4ab
신정연 <ssjy11@gmail.com>
2022-10-06 18:32:30 +0900

    Route 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>   {/* 리액트 라우터 적용을 위해 추가. 페이지를 새로 불러오지 않고도 주소를 변경하고 현재 주소의 경로에 관련된 정보를 리액트 컴포넌트에서 사용할 수 있도록 해줌. */}
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
